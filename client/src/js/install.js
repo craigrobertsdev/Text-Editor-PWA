bf57c38d1bc96f3156e4737c20dc5d3181069cd3
Craig Roberts <craigroberts11@outlook.com>
2023-04-30 05:50:41 +0930

    Added CSS to hide button

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,17 +1,12 @@
 const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener("beforeinstallprompt", (event) => {
   // store the triggered event
-  window.deferredPrompt = event;
-  if (window.matchMedia("(display-mode: standalone)").matches === false) {
-    // toggle install button visbility on
-    butInstall.classList.toggle("hidden", false);
-  }
+  // toggle install button visbility on
+  butInstall.classList.toggle("hidden", false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
   const promptEvent = window.deferredPrompt;
 
@@ -28,7 +23,6 @@ butInstall.addEventListener("click", async () => {
   butInstall.classList.toggle("hidden", true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
   // Clear prompt
   window.deferredPrompt = null;
